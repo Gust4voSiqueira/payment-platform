7778dada1cc3969343d69175fa1ec1bc3b2cede8
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2026-02-04 12:04:18 -0300

    feat: create wallet-service project

---
+++ wallet-service/src/test/java/com/gustavosiqueira/payment/wallet/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavosiqueira.payment.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
