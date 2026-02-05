bc03c5d7b6fc89ba41d2a410c135757c44d9937e
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2026-02-05 14:40:41 -0300

    feat: create fraud-service project

---
+++ fraud-service/src/test/java/com/gustavosiqueira/payment/fraud/FraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavosiqueira.payment.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
