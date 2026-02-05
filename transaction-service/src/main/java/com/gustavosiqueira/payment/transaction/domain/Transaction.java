d4c4e8da9b0154f6ebe3fa98e761d90e2e2a47c3
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2026-02-05 19:40:46 -0300

    fix: @Column in Transaction

--- transaction-service/src/main/java/com/gustavosiqueira/payment/transaction/domain/Transaction.java
+++ transaction-service/src/main/java/com/gustavosiqueira/payment/transaction/domain/Transaction.java
@@ -1,8 +1,7 @@
 package com.gustavosiqueira.payment.transaction.domain;
 
 import com.gustavosiqueira.payment.transaction.adapters.in.controller.dto.CreateTransactionRequest;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,10 +20,14 @@ public class Transaction {
 
     @Id
     private UUID id;
+    @Column(name = "from_wallet_id")
     private UUID fromWalletId;
+    @Column(name = "to_wallet_id")
     private UUID toWalletId;
     private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
     private TransactionStatus status;
+    @Column(name = "created_at")
     private Instant createdAt;
 
     public static Transaction fromCreateTransactionRequest(CreateTransactionRequest createTransactionRequest) {
