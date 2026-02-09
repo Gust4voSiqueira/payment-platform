edf2abf2965e492a954b31fb63f91564356757ed
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2026-02-09 10:29:34 -0300

    feat: create consumerTransaction in TransactionEventConsumer

--- wallet-service/src/main/java/com/gustavosiqueira/payment/wallet/application/ports/in/TransactionEventConsumer.java
+++ wallet-service/src/main/java/com/gustavosiqueira/payment/wallet/application/ports/in/TransactionEventConsumer.java
@@ -1,10 +1,11 @@
 package com.gustavosiqueira.payment.wallet.application.ports.in;
 
 import com.gustavosiqueira.payment.wallet.application.event.TransactionCreatedEvent;
+import org.springframework.messaging.Message;
 
 import java.util.function.Consumer;
 
 public interface TransactionEventConsumer {
 
-    Consumer<TransactionCreatedEvent> transactionCreated();
+    Consumer<Message<TransactionCreatedEvent>> consumerTransaction();
 }
